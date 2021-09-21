2785a126ea355c13c737ec941716a4cc0852c061
Elinor <e.onc@student.tudelft.nl>
2021-09-21 22:52:53 +0200

    Made a circle I guess

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import * as d3 from 'd3'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg width="960" height="500">
+
+        <circle 
+          cx="480"
+          cy="250"
+          r="245"
+          fill="yellow"
+          stroke="black"
+          stroke-width="10"
+        />
+      </svg>
     </div>
   );
 }
